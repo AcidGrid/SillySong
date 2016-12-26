4da17a4f3e49990bc28e14ad8f7cda386701f221
AcidGrid <developer@acidgrid.com>
2016-12-25 20:46:49 -0600

    Initial Commit

---
+++ Silly Song/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Luis Enrique Cadena Fragoso on 25/12/16.
+//  Copyright © 2016 Luis Enrique Cadena Fragoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
